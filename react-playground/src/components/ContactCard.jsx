6e47158f44b769bff047143ea2b22dc78dac9e4d
katherinemorgan-ut <katherinemorgan.ut@gmail.com>
2025-05-08 17:00:04 -0600

    cat contacts 1 - props object

---
+++ react-playground/src/components/ContactCard.jsx
@@ -0,0 +1,28 @@
+function ContactCard(props) {
+
+    return (  
+          <article className="contact-card">
+              <img 
+                  src={props.img}
+                  alt="Photo of Mr. Whiskerson"
+              />
+              <h3>{props.name}</h3>
+              <div className="info-group">
+                  <img 
+                      src="/assets/phone-icon.png" 
+                      alt="phone icon" 
+                  />
+                  <p>{props.phone}</p>
+              </div>
+              <div className="info-group">
+                  <img 
+                      src="/assets/mail-icon.png" 
+                      alt="mail icon"
+                  />
+                  <p>{props.email}</p>
+              </div>
+          </article>
+    )
+}
+
+export default ContactCard
--- react-playground/src/main.jsx
+++ react-playground/src/main.jsx
@@ -2,16 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-/**
- * Challenge:
- * 
- * - Create an App component in a separate file.
- *   Import it here and render it
- * - Create a "components" folder and a Header component.
- *   Render the Header inside the App component.
- * - Follow the Travel Journal design to build the Header
- *   for our page.
- */
 
 const root = createRoot(document.getElementById("root"))
 root.render(< App />)
