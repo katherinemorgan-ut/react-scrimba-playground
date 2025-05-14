863d5f03b613154e70a931161e2e9e5dda49a670
katherinemorgan-ut <katherinemorgan.ut@gmail.com>
2025-05-14 15:01:51 -0600

    react joke app

--- react-playground/src/App.jsx
+++ react-playground/src/App.jsx
@@ -1,41 +1,42 @@
-import ContactCard from "./components/contactCard";
+import Joke from "./components/joke";
 
 /**
- * Challenge:
- * - Create a Contact component in another file
- * - Move one of the contact card articles below into that file
- * - import and render 4 instances of that contact card
- *     - Think ahead: what's the problem with doing it this way?
+ * Challenge: create a page that displays your favorite jokes
+ * - Create a Joke component in its own file.
+ * - Import and render 4-5 <Joke /> components
+ * - Each Joke should receive a "setup" prop and a "punchline" prop
+ *   and render those however you'd like
+ * - Use your favorite 2-part jokes (setup & punchline), or check
+ *   jokes.md file for some examples.
+ *
+ * EXTRA CREDIT:
+ * Some jokes are only a punchline with no setup:
+ *
+ * E.g.: "It’s hard to explain puns to kleptomaniacs because
+ * they always take things literally."
+ *
+ * If you don't pass in a "question" prop, how might you make it only
+ * show the punchline?
  */
 
 function App() {
   return (
-    <div className="contacts">
-      <ContactCard 
-        img="/assets/mrWhiskerson.png"
-        name="Mr. Whiskerson"  
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
+    <>
+      <Joke 
+        setup="Why is 6 afraid of 7?" 
+        joke="Because 7 ate (8) 9!" />
+      <Joke 
+        setup="I got my daughter a fridge for her birthday." 
+        joke="I can't wait to see her face light up when she opens it." />
+      <Joke 
+        setup=" Why do bees stay in the hive in the winter?" 
+        joke="Swarm." />
+      <Joke 
+        setup=""
+        joke="It’s hard to explain puns to kleptomaniacs because 
+        they always take things literally."
       />
-      <ContactCard 
-        img="/assets/fluffykins.png"
-        name="Fluffykins"
-        phone="(212) 555-2345"
-        email="fluff@me.com"
-      />
-      <ContactCard 
-        img="/assets/felix.png"
-        name="Felix"
-        phone="(212) 555-4567"
-        email="thecat@hotmail.com"
-      />
-      <ContactCard 
-        img="/assets/pumpkin.png"
-        name="Pumpkin"
-        phone="(0800) CAT KING"
-        email="pumpkin@scrimba.com"
-      />
-    </div>
+    </>
   );
 }
 
---
+++ react-playground/src/components/joke.jsx
@@ -0,0 +1,11 @@
+function Joke({setup, joke }) {
+  return (
+    <>
+      <h2>{setup}</h2>
+
+      <p>{joke}</p>
+    </>
+  );
+}
+
+export default Joke
