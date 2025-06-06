b73ca15dbb759a81d182ac258ef41ee539f6bb41
katherinemorgan-ut <katherinemorgan.ut@gmail.com>
2025-06-06 17:01:17 -0600

    adds form functionality with action attribute

--- react-playground/src/components/Main.jsx
+++ react-playground/src/components/Main.jsx
@@ -1,13 +1,39 @@
+import React from "react"
+
 function Main() {
+
+
+    const [ingredients, setIngredients] = React.useState([])
+
+
+    const ingredientsMap = ingredients.map(ingredient => {
+        return (
+            <li>{ingredient}</li>
+        )
+    })
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        if(newIngredient !== "") {
+            setIngredients(ingredients => [...ingredients, newIngredient])
+        }
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input aria-label="Add Ingredient"
                  placeholder="Oregano"
-                    type="text" />
+                    type="text" 
+                    name="ingredient"/>
+                    
                 <button>Add Ingredient</button>
 
             </form>
+
+            <ul>
+                {ingredientsMap }
+            </ul>
 
 
         </main>
